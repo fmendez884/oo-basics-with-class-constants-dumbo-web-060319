cb90b90d799d98f1d0147fb1fa05d3e562b22054
Francis Mendez <fmendez884@gmail.com>
2019-06-05 00:26:59 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,4 @@
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
@@ -13,11 +14,6 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-  
-  # def brand=(brand)
-  #   @brand = brand
-  #   BRANDS << brand
-  # end 
 
 end
 
